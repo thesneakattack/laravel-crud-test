8a2abcffd05056f900652f256acd5cec2d7bcdda
David F <davidfdz@gmail.com>
2023-01-23 03:12:20 +0000

    ready to work on nested components for each model

---
+++ app/Http/Livewire/SubCategories/SubCategories.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\SubCategories;
+
+use Livewire\Component;
+
+class SubCategories extends Component
+{
+    public function render()
+    {
+        return view('livewire.sub-categories.sub-categories');
+    }
+}
